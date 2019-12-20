73901bb04a5bfda676fc48cc9ed7c30641fbd21e
Joanna E K <kalemajoanna@gmail.com>
2019-12-20 22:42:39 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,13 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :anagram_as
+  def initialize(anagram_as)
+    @anagram_as = anagram_as
+     end 
+     
+     def match(array_of_possible_anagrams)
+        array_of_possible_anagrams.select do |element|
+            (@anagram_as.split("").sort) == (element.split("").sort)
+        end
+    end
+end 
