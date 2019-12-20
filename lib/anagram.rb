441b7bead08f174a3d8951775ba9405eebe5372d
nwscala <nwscala@gmail.com>
2019-12-20 16:48:44 -0500

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
 # Your code goes here!
+class Anagram
+    attr_accessor :word
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(anagram_array)
+        anagram_array.select do |anagram_word|
+            (@word.split("").sort) == (anagram_word.split("").sort)
+        end
+    end
+end
