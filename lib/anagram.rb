3476332d32040ea5ade068dc1e21bedb245b23be
anupama <anupamagadde@yahoo.com>
2019-12-20 05:07:19 -0600

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+class Anagram
+  
+    attr_accessor :word
+    
+    def initialize(word)
+      @word = word
+      #puts @word#listen
+    end
+    
+    def match(array_words)
+      array_words.select do |w|
+         if  w.split("").sort  == @word.split("").sort
+            word
+         end
+       end
+    end  
+  end
