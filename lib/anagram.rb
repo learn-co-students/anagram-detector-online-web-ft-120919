b73d021208c6b9dee06d1aee05805764402a1417
Scott Hoisington <hoyzington@gmail.com>
2019-12-21 07:33:50 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,21 @@
-# Your code goes here!
+
+class Anagram
+  
+  attr_accessor :word
+  
+  def initialize(word)
+    @word = word
+  end
+
+  def match(words)
+    answer = []
+    comparison = @word.split("").sort
+    words.each do |word|
+      if word.split("").sort == comparison
+        answer << word
+      end
+    end
+    answer
+  end
+
+end
