c15435d7fcbbca0bf4578ada6f686ebe8f8e576d
github username <github email address>
2019-12-21 21:09:50 -0500

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,24 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram
+    attr_accessor :thing
+
+    def initialize(word)
+        @thing = word
+        # @word_array = word_array
+    end
+
+    def match(word_array)
+    #     new_array = []
+    #     #You'll need to iterate over the array of words that the .match method takes as an 
+    #     #argument. You will compare each word of that array to the word that the Anagram class 
+    #     #is initialized with.
+        word_array.select do |word_from_array|
+        # binding.pry
+            word_from_array.split("").sort == @thing.split("").sort
+        end
+    end
+
+
+
+end
