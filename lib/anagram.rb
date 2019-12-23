7872b7a5a6f3b4df60ea85573de144f272d2c52a
Ayako Wiltshire <awittyl44@gmail.com>
2019-12-23 15:32:17 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
-# Your code goes here!
+require 'pry'
+class Anagram
+  
+  attr_accessor :words
+  
+  def initialize(words)
+    
+    @words = words
+  end
+  
+  def match(array)
+    array.select do |word| 
+      @words.split("").sort == word.split("").sort
+    end
+     
+  end
+  
+  
+end
